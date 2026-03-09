8e268a4e56b825e390bbadd62e76538aad287050
zizoubouchelkia <zizoubouchelkia9@gmail.com>
2026-03-09 17:27:30 -0400

    Creation de la classe Parc avec ses attributs

--- atelier3.py
+++ atelier3.py
@@ -8,3 +8,9 @@ class Voiture:
         print("Matricule :", self.matricule)
         print("Marque :", self.marque)
         print("Couleur :", self.couleur)
+class Parc:
+    def __init__(self, id, adresse, capacite):
+        self.id = id
+        self.adresse = adresse
+        self.capacite = capacite
+        self.listeVoitures = []
